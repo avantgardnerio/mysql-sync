23521131cf3c8618afb92cf3938c41dc76bc330a
Brent <bgardner@squarelabs.net>
2021-04-14 12:55:03 -0600

    Can get hashes of source data

--- index.js
+++ index.js
@@ -27,9 +27,21 @@ const argv = yargs
     const fks = await con.awaitQuery(await fs.readFile("db/queries/get_fks.sql", "utf-8"), config.database);
     const cols = await con.awaitQuery(await fs.readFile("db/queries/get_columns.sql", "utf-8"), config.database);
     const tables = cols.reduce((acc, cur) => {
-        acc[cur.TABLE_NAME] = acc[cur.TABLE_NAME] || {"cols": {}};
+        acc[cur.TABLE_NAME] = acc[cur.TABLE_NAME] || {"name": cur.TABLE_NAME, "cols": {}};
         acc[cur.TABLE_NAME].cols[cur.COLUMN_NAME] = cur;
         return acc;
     }, {});
+
+    for(const table of Object.values(tables)) {
+        const md5s = Object.keys(table.cols).map(name => `md5(IFNULL(\`${name}\`, ''))`)
+        let sql = `select 
+                md5(concat(${md5s.join(', ')})) as hash 
+            from \`${table.name}\` 
+            order by hash;`;
+
+        const srcHashes = await con.awaitQuery(sql);
+        console.log(srcHashes)
+    }
+
     con.awaitEnd();
 })();
