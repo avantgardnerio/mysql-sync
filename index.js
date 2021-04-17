5769dd19fba7897f576a41d48e48ffd9e527c0da
Brent <bgardner@squarelabs.net>
2021-04-17 15:43:21 -0600

    never migrations

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ const syncBatch = async (deleteVals, queryVals, dstCon, deleteSql, tableName, pk
         let running = argv['first-table'] === undefined;
         const tableNames = _.intersection(Object.keys(srcTables), Object.keys(dstTables));
         for (const tableName of tableNames) {
-            if(tableName == "migrations") {
+            if(tableName === "migrations") {
                 continue; // never overwrite migrations
             }
             if(argv['skip-tables']?.includes(tableName)) {
